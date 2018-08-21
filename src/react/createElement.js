846624ebded5ab64a828c9250b4b0f8ee1a4b546
xilixjd <349729220@qq.com>
2018-08-22 00:25:19 +0800

    createElement

--- src/react/createElement.js
+++ src/react/createElement.js
@@ -1,10 +1,13 @@
 
 
 function createElement(tag, attrs, ...children) {
+    attrs = attrs || {};
+    
     return {
         tag,
         attrs,
         children,
+        key: attrs.key || null,
     }
 }
 
