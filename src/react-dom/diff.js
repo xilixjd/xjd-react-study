e6963e91883bc020aa24c32a0391916da336eab9
xiejunda <xiejunda@didichuxing.com>
2018-08-21 20:19:02 +0800

    diff

--- src/react-dom/diff.js
+++ src/react-dom/diff.js
@@ -2,6 +2,34 @@ import Component from '../react/component';
 import setAttribute from './dom';
 import { renderComponent } from './render';
 
+function removeNode(node) {
+    if (node && node.parentNode) {
+        node.parentNode.removeChild(node);
+    }
+}
+
+function unmountComponent(component) {
+    if (component.componentWillUnmount) {
+        component.componentWillUnmount();
+    }
+    removeNode(component.base);
+}
+
+function createComponentInst(component, props) {
+    let inst;
+    if (component.prototype && component.prototype.render) {
+        inst = new component(props);
+    } else {
+        inst = new Component(props);
+        inst.constructor = component;
+        inst.render = function() {
+            // 不同
+            return inst.constructor(props);
+        }
+    }
+    return inst;
+}
+
 export function diff(dom, vnode, container) {
     const newDom = diffNode(dom, vnode);
     // newDom.parentNode !== container 这个是否有必要？
@@ -53,10 +81,10 @@ function diffNode(dom, vnode) {
     }
 
     if (vnode.children && vnode.children.length > 0 || out.childNodes && out.childNodes.length > 0) {
-        diffComponent(out, vnode.children);
+        diffChildren(out, vnode.children);
     }
 
-    // out ?
+    // out 没法解决，因为 dom 为 undefined
     diffAttributes(out, vnode);
 
     return out;
@@ -71,4 +99,111 @@ function isSameNodeType(dom, vnode) {
     } else {
         return dom && dom._component && dom._component.constructor === vnode.tag;
     }
+}
+
+function diffChildren( dom, vchildren ) {
+
+    const domChildren = dom.childNodes;
+    const children = [];
+
+    const keyed = {};
+
+    if ( domChildren.length > 0 ) {
+        for ( let i = 0; i < domChildren.length; i++ ) {
+            const child = domChildren[ i ];
+            const key = child.key;
+            if ( key ) {
+                keyed[ key ] = child;
+            } else {
+                children.push( child );
+            }
+        }
+    }
+
+    if ( vchildren && vchildren.length > 0 ) {
+
+        let min = 0;
+        let childrenLen = children.length;
+
+        for ( let i = 0; i < vchildren.length; i++ ) {
+
+            const vchild = vchildren[ i ];
+            const key = vchild.key;
+            let child;
+
+            if ( key ) {
+
+                if ( keyed[ key ] ) {
+                    child = keyed[ key ];
+                    keyed[ key ] = undefined;
+                }
+
+            } else if ( min < childrenLen ) {
+
+                for ( let j = min; j < childrenLen; j++ ) {
+
+                    let c = children[ j ];
+
+                    if ( c && isSameNodeType( c, vchild ) ) {
+
+                        child = c;
+                        children[ j ] = undefined;
+
+                        if ( j === childrenLen - 1 ) childrenLen--;
+                        if ( j === min ) min++;
+                        break;
+
+                    }
+
+                }
+
+            }
+
+            child = diffNode( child, vchild );
+
+            const f = domChildren[ i ];
+            if ( child && child !== dom && child !== f ) {
+                if ( !f ) {
+                    console.log("appendChild", child, vchild)
+                    dom.appendChild(child);
+                } else if ( child === f.nextSibling ) {
+                    console.log("remove", f)
+                    removeNode( f );
+                } else {
+                    console.log("insertbefore", child)
+                    dom.insertBefore( child, f );
+                }
+            }
+
+        }
+    }
+}
+
+function diffComponent(dom, vnode) {
+    let domComp = dom && dom._component;
+    let newDom;
+
+    if (domComp && domComp.constructor === vnode.tag) {
+        setComponentProps(domComp, vnode.attrs);
+        renderComponent(domComp);
+        newDom = domComp.base;
+    } else {
+        if (domComp) {
+            // dom 存在 component（该节点为 component 节点）
+            unmountComponent(domComp);
+        }
+        domComp = createComponentInst(vnode.tag, vnode.attrs);
+        setComponentProps(domComp, vnode.attrs);
+        renderComponent(domComp);
+
+        newDom = domComp.base;
+        if (dom && dom._component) {
+            dom._component = null;
+        }
+        if (dom && newDom !== dom) {
+            removeNode(dom);
+        }
+    }
+
+    return newDom;
 }
