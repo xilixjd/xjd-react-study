9cac956a6b44846097fa4d8b2adf7cb2e4efd193
xiejunda <xiejunda@didichuxing.com>
2018-09-06 10:01:57 +0800

    console

--- src/index.js
+++ src/index.js
@@ -20,16 +20,17 @@ class Counter extends React.Component {
     onClick() {
         let flag = this.state.flag;
         this.setState({ num: this.state.num + 1, flag: !flag });
+        this.setState({ num: this.state.num + 1, flag: !flag });
     }
 
     render() {
         let flag = this.state.flag;
         return (
             <div>
-                {/* {flag ? <Welcome /> : <h1>flag: false</h1>} */}
+                {flag ? <Welcome /> : <h1>flag: false</h1>}
                 {/* {flag ? <h1>flag: true</h1> : <h1>flag: false</h1>} */}
                 {/* <h2>{this.props.a}</h2> */}
-                <h1>count: {this.state.num}</h1>
+                {/* <h1>count: {this.state.num}</h1> */}
                 <button onClick={() => this.onClick()}>add</button>
             </div>
         );
