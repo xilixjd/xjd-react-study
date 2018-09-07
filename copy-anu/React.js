d8d393d93dd3086846e7ef93ad30c82be8b4d28f
xiejunda <xiejunda@didichuxing.com>
2018-09-07 19:10:06 +0800

    options

--- copy-anu/React.js
+++ copy-anu/React.js
@@ -1,5 +1,27 @@
 var pendingRefs = []
 
+var dirtyComponent = []
+
+var nullFunc = function() {}
+
+var option = {
+    beforeUnmount: nullFunc,
+    afterMount: nullFunc,
+    afterUpdate: nullFunc,
+}
+options.flushBatchedUpdates = function (queue) {
+    clearRefsAndMounts(queue || dirtyComponents);
+}
+options.enqueueUpdate = function (instance) {
+    dirtyComponents.push(instance);
+}
+
+function mountVnode(vnode, context, prevRendered, mountQueue) {
+    return 
+}
+
+
+
 function render(vnode, container) {
     return renderByXjdReact(vnode, container)
 }
@@ -7,17 +29,23 @@ function render(vnode, container) {
 function renderByXjdReact(vnode, container) {
     var parentContext = {}
     var mountQueue = []
-    var rootNode = genVnodes(vnode, container, parentContext, mountQueue)
+    var prevRendered = null
+    var rootNode = mountVnode(vnode, parentContext, prevRendered, mountQueue)
+    container.appendChild(rootNode)
     container.__component = vnode
-    // clearRefs componentDidMount 生命周期
     clearRefsAndMounts(mountQueue)
     return rootNode
 }
 
+// clearRefs componentDidMount 生命周期
 function clearRefsAndMounts(queue) {
     var refs = pendingRefs.slice(0)
     pendingRefs.length = 0
+    refs.forEach(function(fn) {
+        fn()
+    })
     queue.forEach(function(instance) {
+        // 只用于第一次 Mount 的时候调用，之后设为 null
         if (instance.componentDidMount) {
             instance.componentDidMount()
             instance.componentDidMount = null
@@ -27,4 +55,6 @@ function clearRefsAndMounts(queue) {
             _refeshComponent(instance, instance.__current._hostNode, [])
         }
     })
+    queue.length = 0
 }
+
